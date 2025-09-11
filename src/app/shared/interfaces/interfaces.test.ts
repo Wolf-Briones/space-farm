92b115d50c5ce2f69802d2cb9af9f45ac186709b
Franki Briones <fbrionesp93@gmail.com>
2025-09-10 23:49:16 -0500

    feat:FRANKI LT changes in to game component

---
+++ src/app/shared/interfaces/interfaces.test.ts
@@ -0,0 +1,36 @@
+
+export interface Resource {
+  type: 'water' | 'energy' | 'biomass' | 'research';
+  value: string;
+  icon: string;
+}
+
+export interface SatelliteData {
+  soilHumidity: number;
+  vegetationIndex: number;
+  soilTemperature: number;
+  precipitation: number;
+}
+
+export interface WeatherData {
+  temperature: number;
+  humidity: number;
+  windSpeed: number;
+  uvIndex: number;
+  pressure: number;
+}
+
+export interface CropInfo {
+  type: string;
+  growth: number;
+  health: string;
+  daysToHarvest: number;
+  estimatedYield: number;
+}
+
+export interface FarmPlot {
+  id: number;
+  planted: boolean;
+  irrigated: boolean;
+  cropIcon: string;
+}
