25b3d31cf323b2e345793f25c96f6cb5bbc3900d
Franki Briones <156685894+Wolf-Briones@users.noreply.github.com>
2025-09-10 20:18:36 -0500

    Update firebase.config.ts

--- src/environment/firebase.config.ts
+++ src/environment/firebase.config.ts
@@ -1,11 +1,5 @@
 export const environment = {
     production: false,
     firebaseConfig: {
-        apiKey: "AIzaSyByGay6K2FSyKW5XUFGvS5NxsfbUoQcufw",
-        authDomain: "spacefamapp.firebaseapp.com",
-        projectId: "spacefamapp",
-        storageBucket: "spacefamapp.firebasestorage.app",
-        messagingSenderId: "708555484902",
-        appId: "1:708555484902:web:27d56cacf024bc26bbd527"
     }
 };
